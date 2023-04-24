0ab4d578e85311a03e391894571210baa9e32c8d
afrejanas <afrejanas@gmail.com>
2023-04-24 12:25:59 -0700

    putdigit function that prints the digit puted and return the length of the digit

---
+++ 1-function.c
@@ -0,0 +1,50 @@
+include"main.h"
+/**
+ * putdigit - printf int
+ *
+ * Return: 0 success
+ */
+
+int putdigit(va_list ap)
+{
+	int n, num, i, exp;
+	int last, digit;
+	
+	i = 1;
+	exp = 1;
+	n = va_arg(ap, int);	
+	last = n % 10;
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_pucthar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num % 10;
+		}
+		num = n;
+
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = digit % exp;
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+	
+	return (i);
+}
