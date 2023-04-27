c3df93bb59416f0d47a28cce3cbb49ce8e70c4fb
afrejanas <afrejanas@gmail.com>
2023-04-26 18:49:14 -0700

    fixed

--- 4-function.c
+++ 4-function.c
@@ -6,25 +6,18 @@
  */
 int print_S(va_list ap)
 {
-	int i, j, c = 0;
+	int i, c = 0;
 	char *str = va_arg(ap, char*);
-	char *res, a;
+	char *res;
 
 	if (str == NULL)
-	{
-		a = "(null)"
-		for (k = 0; a[k]; k++)
-		{
-			_putchar(str[k]);
-			c++;
-		}
-	}
+		return (_puts("(null)"));
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] > 0 && (str[i] < 32 || str[i] >= 127))
 		{
-			_putchar('\\');
-			_putchar('x');
+			_puts("\\x");
 			c = c + 2;
 			res = convert_hex((unsigned long int)str[i], 16, 0);
 			if (!res[1])
@@ -32,11 +25,7 @@ int print_S(va_list ap)
 				c++;
 				_putchar('0');
 			}
-			for (j = 0; res[j] != '\0'; j++)
-			{
-				_putchar(res[j]);
-				c++;
-			}
+				c = c + _puts(res);
 		}
 		else
 		{
--- main.h
+++ main.h
@@ -33,6 +33,7 @@ int print_hex(va_list ap);
 char *convert_hex(unsigned long int num, int base, int l);
 int print_hex_big(va_list ap);
 int print_S(va_list ap);
+int _puts(char *str);
 
 
 
